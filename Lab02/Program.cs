c9e4cb62b30dd65209a0699f6c9bbca906b586d3
dakanda.jakn <dakanda.jakn@bumail.net>
2026-08-28 00:14:29 +0700

    GI113-2026-1670317146 (2)
    Edit code

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -46,47 +46,55 @@ namespace Lab02
             char rank1 = 'S';
             int level1 = 15;
             int maxHp1 = 8800;
+            float damage1 = 569.9f;
 
             Console.WriteLine("Character01");
             Console.WriteLine($"Name: {Name1}");
             Console.WriteLine($"Rank: {rank1}");
             Console.WriteLine($"Level: {level1}");
             Console.WriteLine($"Hp: {maxHp1}");
+            Console.WriteLine($"Damage: {damage1}");
             Console.WriteLine();
 
             string name2 = "Rouei";
             float damage = 57.9f;
             double critDamage = 9.55;
             char rank2 = 'S';
+            int maxHp2 = 12000;
 
             Console.WriteLine("Character02");
             Console.WriteLine($"Name: {name2}");
             Console.WriteLine($"Rank: {rank2}");
             Console.WriteLine($"Damage: {damage}");
             Console.WriteLine($"CritDamge: {critDamage}");
+            Console.WriteLine($"Hp: {maxHp2}");
             Console.WriteLine();
 
             string Name3 = "MaxNum";
             char rank3 = 'A';
             int level2 = 15;
-            int maxHp2 = 13000;
+            int maxHp3 = 13000;
+            int Damage = 500;
 
             Console.WriteLine("Character03");
             Console.WriteLine($"Name: {Name3}");
             Console.WriteLine($"Rank: {rank2}");
             Console.WriteLine($"Level: {level2}");
-            Console.WriteLine($"Hp: {maxHp2}");
+            Console.WriteLine($"Hp: {maxHp3}");
+            Console.WriteLine($"Damage: {Damage}");
             Console.WriteLine();
 
             string Name4 = "Zip";
             char rank4 = 'A';
-            int maxHp3 = 15000;
+            int maxHp4 = 15000;
             bool isTank = true;
+            int level3 = 15;
 
             Console.WriteLine("Character04");
             Console.WriteLine($"Name: {Name4}");
-            Console.WriteLine($"Hp: {maxHp3}");
+            Console.WriteLine($"Hp: {maxHp4}");
             Console.WriteLine($"isTank?: {isTank}");
+            Console.WriteLine($"Level: {level3}");
         }
     }
 }
